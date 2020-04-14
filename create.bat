07c7eb372fae13ca83eac4640b75531955b900d5
dibasbehera <dibasbehera@gmail.com>
2020-04-14 17:59:37 +0530

    Update create.bat
    exception handled

--- create.bat
+++ create.bat
@@ -5,13 +5,14 @@ set flag=%2
 cd /d %~dp0
 
 If "%1"=="" (
-    echo "error : please enter project name wisely"
+    echo "error executing  %0 command : Please enter project name wisely"
 ) else ( 
-    if "%2"=="g" (
-        python remote.py %fn% %flag%
-        ) else (
-            if "%2"=="l" (
-                python local.py %fn%
-            )
+    if "%2" == "" (
+        echo "error executing  %0 %1 command : Please enter locally(l) or globally(g)"
+    ) else (
+        if "%2"=="g" (python remote.py %fn% %flag%) else (
+            if "%2"=="l" (python local.py %fn% %flag%) else (
+                echo "%0 %1 %2 command fails: Please enter locally(l) or globally(g)")) 
         )
     )
+
